71b0b210fcf837b0d612f7c3fe0c03392837c10e
StartStatic1 <startstatic@outlook.com>
2026-09-08 09:29:17 -0300

    ViewModel: mais fontes de proxy (ProxyScrape, mmpx12, jetkai, proxifly)

--- android-native/app/src/main/java/com/ascan/ascanagent/ui/ScanViewModel.kt
+++ android-native/app/src/main/java/com/ascan/ascanagent/ui/ScanViewModel.kt
@@ -191,11 +191,16 @@ class ScanViewModel(app: Application) : AndroidViewModel(app) {
             proxyLoading = true
             log("Baixando proxies...")
             val urls = listOf(
-                "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=5000&country=all",
-                "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt",
+                "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=3000&country=all&ssl=all&anonymity=all",
+                "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=5000&country=BR,US,DE,NL,FR",
+                "https://raw.githubusercontent.com/mmpx12/proxy-list/master/http.txt",
+                "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-http.txt",
                 "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt",
+                "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt",
                 "https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt",
-                "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/http.txt"
+                "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/http.txt",
+                "https://raw.githubusercontent.com/roosterkid/openproxylist/main/HTTPS_RAW.txt",
+                "https://raw.githubusercontent.com/proxifly/free-proxy-list/main/proxies/protocols/http/data.txt"
             )
             val found = mutableListOf<String>()
             withContext(Dispatchers.IO) {
