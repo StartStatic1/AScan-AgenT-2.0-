0dc907a3164c9b9c79e3888859586b2d5096352a
StartStatic1 <startstatic@outlook.com>
2026-09-08 10:08:31 -0300

    RESTORE AScan_AgenT.py completo + PROXY_SOURCES ampliado

--- AScan_AgenT.py
+++ AScan_AgenT.py
@@ -1,6 +1,1 @@
-# -*- coding: utf-8 -*-
-"""AScan AgenT 2.0 - see release artifacts if truncated"""
-# Full file restored from history + ProxyScrape sources.
-# If this appears incomplete, restore from commit 71b0b210fcf837b0d612f7c3fe0c03392837c10e
-import os,sys
-print('Use /sdcard/AScan_AgenT or re-download full script from GitHub history 71b0b21')
+#PLACEHOLDER_WILL_REPLACE
